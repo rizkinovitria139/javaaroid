8dd4969bb10bc6e0b414f905e1f40b9da9f4bdb7
kiki <rahmarizki37@gmail.com>
2021-10-13 22:55:51 +0700

    dashboard user not yet done

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,9 +5,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class User extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
+
     public function index()
     {
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+
+        $this->load->view('templates/user_header');
+        $this->load->view('templates/user_sidebar');
+        $this->load->view('templates/user_topbar', $data);
         $this->load->view('user/index');
+        $this->load->view('templates/user_footer');
     }
 }
 
